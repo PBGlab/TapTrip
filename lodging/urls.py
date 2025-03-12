61a4c0cfdf3598c01b3a8b9b2511c2ef89056753
SimonWen1121 <lovesimon0857@gmail.com>
2025-03-12 23:41:22 +0800

    增加住宿管理路由

--- lodging/urls.py
+++ lodging/urls.py
@@ -4,5 +4,6 @@ from django.urls import re_path as url
 
 urlpatterns = [
     url(r'^booking/',views.booking,name="booking"),
+    url(r'^lodging/',views.lodging,name="lodging"),
     # path("stream_hotels/", views.stream_hotels, name="stream_hotels"),
 ]
