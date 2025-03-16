2ae036348ba8401a2950e0c87ac1db1cf920f2b9
SimonWen1121 <lovesimon0857@gmail.com>
2025-03-16 17:19:08 +0800

    新增預定、刪除路由

--- lodging/urls.py
+++ lodging/urls.py
@@ -3,8 +3,10 @@ from lodging import views
 from django.urls import re_path as url
 
 urlpatterns = [
-    url(r'^booking/',views.booking,name="booking"),
-    url(r'^lodging/',views.lodging,name="lodging"),
-    path('stream-hotels/', views.stream_hotels, name='stream_hotels'),  # SSE 路由
-    path('showhotel/', views.showhotel, name='showhotel'),
+    url(r'^booking/',views.booking,name="booking"), #定房畫面
+    url(r'^lodging/',views.lodging,name="lodging"), #管理住宿頁面
+    path('stream-hotels/', views.stream_hotels, name='stream_hotels'),  # SSE 路由 傳遞爬蟲結果
+    path('showhotel/', views.showhotel, name='showhotel'), #搜尋結果
+    path("book_hotel/", views.book_hotel, name="book_hotel"), #預定飯店
+    path("delete_lodging/<int:lodging_id>/", views.delete_lodging, name="delete_lodging") #刪除飯店路由
 ]
