ee7697a7cdc76a046182cbc2ee4996c52e6e4350
SimonWen1121 <lovesimon0857@gmail.com>
2025-03-21 17:13:23 +0800

    註冊行程、行程日期

--- trips/admin.py
+++ trips/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Trip, TripDay
 
-# Register your models here.
+
+@admin.register(Trip)
+class TripAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'start_date', 'end_date', 'status')
+
+
+@admin.register(TripDay)
+class TripDayAdmin(admin.ModelAdmin):
+    list_display = ('trip', 'date')
+    
