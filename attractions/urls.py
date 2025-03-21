7ecf04b53be400e8e5e70a112e8911d7ae1a6904
SimonWen1121 <lovesimon0857@gmail.com>
2025-03-21 17:01:29 +0800

    新增"編輯景點"路由

--- attractions/urls.py
+++ attractions/urls.py
@@ -4,4 +4,5 @@ from django.urls import re_path as url
 
 urlpatterns = [
     url(r'^findattractions/',views.findattractions,name="findattractions"),
+    url(r'^showattractions/',views.showattractions,name="showattractions"),
 ]
