1471d0e3b7a2f4bad516b9f90191647b3f36da9f
Ubuntu <ubuntu@ip-172-31-14-64.ap-southeast-2.compute.internal>
2025-03-31 19:53:00 +0000

    按鈕交前存檔

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +31,7 @@ SECRET_KEY = "django-insecure-b0iu&uj@f7+gzjq6fwla04496)gmdgw8g1llb88ye&fej^dq1=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['taptrip2025.com','www.taptrip2025.com','127.0.0.1']
+ALLOWED_HOSTS = ['taptrip2025.com','www.taptrip2025.com','127.0.0.1','13.236.187.118',]
 
 
 # Application definition
@@ -119,7 +123,7 @@ DATABASES = {
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
 
-        
+
         # "ENGINE": "django.db.backends.sqlite3",
         # "NAME": BASE_DIR / "db.sqlite3",
     }
